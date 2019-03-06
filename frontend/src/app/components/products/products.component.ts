4252bebcc84dead15dcd03a38dd8fce2a1abd482
erikbg7 <erik.bg7@gmail.com>
2019-03-06 15:05:57 +0100

    Added function to delete product from DDBB

--- frontend/src/app/components/products/products.component.ts
+++ frontend/src/app/components/products/products.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ProductService} from "../../services/product.service";
 import {Product} from "../../models/product";
 import {Router} from "@angular/router";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-products',
@@ -23,6 +24,35 @@ export class ProductsComponent implements OnInit {
       .subscribe(res =>{
         this.products = res["products"];
       });
+  }
+
+  /**
+   *
+   * @param id
+   */
+  confirmDelete(id: string) {
+    if(confirm('El producto se borrará de tu lista de productos...')){
+      this.productService.deleteProduct(id)
+        .subscribe(
+          res =>{
+            console.log(res);
+            alert("Se ha borrado correctamente");
+            this.getProducts();
+          },
+          err => {
+            this.handleError(err);
+          });
+    }
+  }
+
+  /**
+   *
+   * @param err
+   */
+  private handleError(err: HttpErrorResponse) {
+    if( err.status == 500 ) {
+      alert(err);
+    }
   }
 
   goBack() {
